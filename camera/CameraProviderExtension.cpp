f71cd32f077497947d446187925ebf0ee0cf82c0
Dhina17 <dhinalogu@gmail.com>
2024-05-27 19:19:11 +0530

    sm8250-common: Implement torch light control Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    [RealJohnGalt:] modify for oneplus usage, add toggle switch

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -53,6 +53,15 @@ int32_t getTorchDefaultStrengthLevelExt() {
 int32_t getTorchMaxStrengthLevelExt() {
     // 200 (out of 500) is a sane max brightness
     return 200;
+int32_t getTorchDefaultStrengthLevelExt() {
+    return 7;
+}
+
+int32_t getTorchMaxStrengthLevelExt() {
+    // In our device, both LEDs has same maximum value
+    // so get from one.
+    auto node = kTorchLedPaths[0] + "/" + TORCH_MAX_BRIGHTNESS;
+    return get(node, 0);
 }
 
 int32_t getTorchStrengthLevelExt() {
@@ -75,4 +84,7 @@ void setTorchStrengthLevelExt(int32_t torchStrength, bool enabled) {
 void setTorchModeExt(bool enabled) {
     int32_t strength = getTorchDefaultStrengthLevelExt();
     setTorchStrengthLevelExt(enabled ? strength : 0, enabled);
+}
+    if (torchStrength > 0)
+        set(TOGGLE_SWITCH, 255);
 }
