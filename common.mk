a2000f4ff3e6620d9a2852fcb00fb40cd4f5f279
bijoyv9 <bijoyv9@gmail.com>
2025-10-04 18:07:29 +0000

    sm8250-common: Initialize RealmeAddons
    Change-Id: I55d95036ee50b98ba610c4825011c9e7c6d54944

--- common.mk
+++ common.mk
@@ -119,7 +119,8 @@ PRODUCT_COPY_FILES += \
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
+    DSPVolumeSynchronizer \
+    RealmeAddons
 
 # Display
 PRODUCT_PACKAGES += \
